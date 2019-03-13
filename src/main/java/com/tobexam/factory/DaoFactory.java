416993190e98021253b823eeb96dca68febd8802
psk <psk3060@naver.com>
2019-03-13 19:29:47 +0900

    DaoFactory 의존성 수정(ConnectionBean)

---
+++ src/main/java/com/tobexam/factory/DaoFactory.java
@@ -0,0 +1,47 @@
+package com.tobexam.factory;
+
+import com.tobexam.common.*;
+import com.tobexam.dao.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DaoFactory {
+    @Bean
+    public UserDao userDao() {
+        UserDao dao = null;
+
+        dao = new UserDao(connectionMaker());
+
+        return dao;
+    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        XMLParsingConfig parConfig = new XMLParsingConfig();
+        ConnectionMaker connectionMaker = null;
+
+        try {
+            ConnectionBean mysqlConfig1 = makeConnBean("mysql_conn.xml");
+
+            connectionMaker = new ConcreConnectionMaker(mysqlConfig1);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return connectionMaker;
+    }
+
+    @Bean
+    public ConnectionBean makeConnBean(String fileName) {
+        XMLParsingConfig parConfig = new XMLParsingConfig();
+        ConnectionBean conConfig = null;
+        try {
+            conConfig = parConfig.setConfig(fileName);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return conConfig;
+    }
+
+}
