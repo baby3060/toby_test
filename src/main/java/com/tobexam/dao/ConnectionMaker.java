04f64b2cec5a17ed74d1f27b85342b9ef164a5dc
psk <psk3060@naver.com>
2019-03-13 19:39:31 +0900

    필요없는 import 정리

--- src/main/java/com/tobexam/dao/ConnectionMaker.java
+++ src/main/java/com/tobexam/dao/ConnectionMaker.java
@@ -3,6 +3,8 @@ package com.tobexam.dao;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import com.tobexam.common.*;
+
 public interface ConnectionMaker {
     public Connection getConnection() throws ClassNotFoundException, SQLException;
 }
