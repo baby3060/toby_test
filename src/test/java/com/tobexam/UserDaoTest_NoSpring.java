ed1048833eb5a0fd5a47c202ca28407af0c3849f
psk <psk3060@naver.com>
2019-03-14 20:43:19 +0900

    스프링 사용하지 않은 테스트

---
+++ src/test/java/com/tobexam/UserDaoTest_NoSpring.java
@@ -0,0 +1,81 @@
+package com.tobexam;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.tobexam.common.*;
+import com.tobexam.model.*;
+import com.tobexam.dao.*;
+
+/**
+ * 스프링 없이 실행한 테스트
+ */
+public class UserDaoTest_NoSpring {
+    private ConnectionBean connBean;
+    private XMLParsingConfig config;
+
+    private UserDao userDao;
+
+    @Before
+    public void setUp() {
+        config = new XMLParsingConfig();
+        config.setFileName("mysql_conn.xml");
+        try {
+            connBean = config.setConfig();
+
+            userDao = new UserDao();
+
+            DataSource dataSource = new SingleConnectionDataSource(connBean.getConnStr(), connBean.getUserName(), connBean.getUserPass(), true);
+            userDao.setDataSource(dataSource);
+        } catch(Exception e) {
+
+        }
+        
+    }
+
+    @Test
+    public void addAndGet2() {
+        User user = new User();
+        
+        try {
+            int result = userDao.deleteAll();
+            int count = userDao.countAll();
+            
+            assertThat(userDao.countAll(), is(0));
+            
+            user = new User("1234", "1234", "12345");
+
+            result = userDao.add(user);
+
+            assertThat(userDao.countAll(), is(1));
+
+            User user2 = userDao.get("1234");
+
+            assertThat(user2.getName(), is(user.getName()));
+            assertThat(user2.getPassword(), is(user.getPassword()));
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test(expected=EmptyResultDataAccessException.class)
+    public void getUserFailure2() throws Exception, SQLException {
+        
+        // 예외 발생
+        // User user = userDao.get("1234");
+        // 예외가 안 발생할 때는? => 테스트 실패
+        User user = userDao.get("1234");
+    }
+
+}
