c1beff87b5574257ae2207fe8bce275ce779b3bd
psk <psk3060@naver.com>
2019-03-14 18:42:32 +0900

    Test를 위한 생성자 추가

--- src/main/java/com/tobexam/model/User.java
+++ src/main/java/com/tobexam/model/User.java
@@ -10,4 +10,12 @@ public class User {
     @NonNull private String id;
     @NonNull private String name;
     @NonNull private String password;
+
+    public User() { }
+
+	public User(String id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
 }
