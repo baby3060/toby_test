fc3967ec55d9a4ad47222d6ebc6442c2787cb288
psk <psk3060@naver.com>
2019-03-16 13:34:48 +0900

    Context 로 이동 완료(Dao)

--- src/main/java/com/tobexam/dao/UserDao_Mod.java
+++ src/main/java/com/tobexam/dao/UserDao_Mod.java
@@ -42,172 +42,23 @@ public class UserDao_Mod {
         this.jdbcContext.executeSql("Delete From USER Where id = ?", user.getId());
     }
 
-    public User get(String id) throws EmptyResultDataAccessException, Exception {
-        
-        User user = null;
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            String sql = "Select id, name, password From USER Where id = ? ";
-
-            conn = dataSource.getConnection();
-
-            pstmt = conn.prepareStatement(sql);
-
-            pstmt.setString(1, id);
-
-            rs = pstmt.executeQuery();
-
-            if( rs.next() ) {
-                user = new User();
-
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-            }
-
-            if( user == null ) {
-                throw new EmptyResultDataAccessException(1);
-            }
-        } catch(SQLException e ) {
-            throw new Exception(e);
-        } finally {
-            if(rs != null) {
-                try {
-                    rs.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if(pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(conn != null) {
-                try {
-                    conn.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return user;
+    public int count(String id) throws Exception {
+        int count = this.jdbcContext.executeQueryOneInt("Select Count(*) As cnt From USER Where id = ? ", id);
+        return count;
     }
 
     public int countAll() throws Exception {
         int count = this.jdbcContext.executeQueryOneInt("Select Count(*) As cnt From USER");
-
         return count;
     }
 
-    public int countAll(String id) throws Exception {
-        int count = 0;
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            String sql = "Select Count(*) As cnt From USER Where id = ? ";
-
-            conn = dataSource.getConnection();
-
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, id);
-            rs = pstmt.executeQuery();
-
-            if( rs.next() ) {
-                count = rs.getInt("cnt");
-            }
-        } catch(SQLException e ) {
-            throw new Exception(e);
-        } finally {
-            if(rs != null) {
-                try {
-                    rs.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if(pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(conn != null) {
-                try {
-                    conn.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return count;
+    public User get(String id) throws EmptyResultDataAccessException, Exception {
+        User user = this.jdbcContext.executeQueryOneObject("Select id, name, password From USER Where id = ? ", User.class, id);
+        return user;
     }
 
     public List<User> selectAll() throws Exception {
-        List<User> userList = new ArrayList<User>();
-        User user = null;
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            String sql = "Select id, name, password From USER ";
-
-            conn = dataSource.getConnection();
-
-            pstmt = conn.prepareStatement(sql);
-
-            rs = pstmt.executeQuery();
-
-            while( rs.next() ) {
-                user = new User();
-
-                user.setId(rs.getString("id"));
-                user.setName(rs.getString("name"));
-                user.setPassword(rs.getString("password"));
-
-                userList.add(user);
-            }
-
-        } catch(SQLException e ) {
-            throw new Exception(e);
-        } finally {
-            if(rs != null) {
-                try {
-                    rs.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if(pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if(conn != null) {
-                try {
-                    conn.close();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
+        List<User> userList = this.jdbcContext.executeQueryList("Select id, name, password From USER", User.class);
         return userList;
     }
 }
