488aa17c8ba0234615cc8a1e8b84c43b8267ffbf
psk <psk3060@naver.com>
2019-03-18 15:22:17 +0900

    UserService Test 수정

--- src/test/java/com/tobexam/UserServiceTest.java
+++ src/test/java/com/tobexam/UserServiceTest.java
@@ -15,6 +15,9 @@ import com.tobexam.dao.*;
 import com.tobexam.service.*;
 import org.junit.runner.RunWith;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -26,58 +29,74 @@ import org.springframework.test.context.ContextConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/applicationContext.xml")
 public class UserServiceTest {
+    static class TestUserService extends UserService {
+        private String id;
+    
+        private TestUserService(String id) {
+            this.id = id;
+        }
+        
+        protected void upgradeLevel(User user) {
+            if(user.getId().equals(this.id)) {
+                throw new TestUserServiceException();
+            } else {
+                super.upgradeLevel(user);
+            }
+        }
+    }
+    
+    static class TestUserServiceException extends RuntimeException { }
+
     List<User> users;
 
+    @Autowired
     private UserService userService;
+    @Autowired
     private UserDao userDao;
+    @Autowired
+    private DataSource dataSource;
 
     @Before
     public void setUp() {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
-        userService = context.getBean("userService", UserService.class);
-
         users = new ArrayList<User>(Arrays.asList(
             new User("1", "김길동", "비번1", Level.BASIC, 49, 0, "a@n.com"),
-            new User("2", "배길동", "비번2", Level.SILVER, 60, 29, "b@n.com"),
+            new User("2", "배길동", "비번2", Level.SILVER, 60, 31, "b@n.com"),
             new User("3", "이길동", "비번3", Level.BASIC, 45, 0, "c@n.com"),
             new User("4", "고길동", "비번4", Level.GOLD, 60, 33, "d@n.com"),
             new User("5", "최길동", "비번5", Level.SILVER, 54, 30, "e@n.com"),
             new User("6", "박길동", "비번6", Level.BASIC, 51, 0, "f@n.com")
         ));
-
-        userDao = context.getBean("userDao", UserDaoJdbc_Template.class);
     }
 
     @Test
     public void upgradeLevels() {
-        try {
+        
             userService.deleteAll();
 
             for(User user : users) {
                 userService.add(user);
             }
 
-            userService.upgradeLevels();
+            try {
+                userService.upgradeLevels();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+            
 
             checkLevelUpgraded(users.get(0), false);
-            checkLevelUpgraded(users.get(1), false);
+            checkLevelUpgraded(users.get(1), true);
             checkLevelUpgraded(users.get(2), false);
             checkLevelUpgraded(users.get(3), false);
             checkLevelUpgraded(users.get(4), true);
             checkLevelUpgraded(users.get(5), true);
-        } catch(Exception e) {
-
-        }
+        
     }
 
     private void checkLevelUpgraded(User user, boolean upgraded) {
-        User userUpgrade =null;
-        try {
-            userUpgrade = userDao.get(user.getId());
-        } catch(Exception e) {
-
-        }
+        User userUpgrade = userDao.get(user.getId());
         
         if( upgraded ) {
             assertThat(userUpgrade.getLevel(), is(user.getLevel().nextLevel()));
@@ -89,29 +108,39 @@ public class UserServiceTest {
 
     @Test
     public void add() {
-        try {
-            userService.deleteAll();
+        
+        userService.deleteAll();
+        User userWithLevel = users.get(0);
+        User userWithoutLevel = users.get(4);
+        userWithoutLevel.setLevel(null);
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithOutLevelRead = userDao.get(userWithoutLevel.getId());
+        assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+        assertThat(userWithOutLevelRead.getLevel(), is(Level.BASIC));
 
-            User userWithLevel = users.get(0);
-            User userWithoutLevel = users.get(4);
-            userWithoutLevel.setLevel(null);
-
-            userService.add(userWithLevel);
-            userService.add(userWithoutLevel);
-
-            User userWithLevelRead = userDao.get(userWithLevel.getId());
-            User userWithOutLevelRead = userDao.get(userWithoutLevel.getId());
-
-
-            assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
-            assertThat(userWithOutLevelRead.getLevel(), is(Level.BASIC));
-
-        } catch(Exception e) {
-
-        }
     }
 
-    
+    @Test
+    public void upgradeAllOrNothing() throws Exception {
+        UserService testUserService = new TestUserService(users.get(4).getId());
+        testUserService.setUserDao(this.userDao);
+        testUserService.setDataSource(this.dataSource);
+        userDao.deleteAll();
 
+        for(User user : users) {
+            userDao.add(user);
+        }
 
+        try {
+            testUserService.upgradeLevels();
+
+            fail("TestUserServiceException expected");
+        } catch(TestUserServiceException e) {
+            e.printStackTrace();
+        }
+
+        checkLevelUpgraded(users.get(1), false);
+    }
 }
