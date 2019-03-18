871041d57c88f8840648872fcf7e3a4ae0d0123e
psk <psk3060@naver.com>
2019-03-18 15:21:18 +0900

    Main 제거

--- src/main/java/com/tobexam/App.java
+++ src/main/java/com/tobexam/App.java
@@ -13,29 +13,6 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class App {
     public static void main( String[] args ) {
-        
-        User user = new User();
-        
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao userDao = context.getBean("userDao", UserDaoJdbc_Template.class);
-        UserService userService = context.getBean("userService", UserService.class);
-        try {
-            userDao.deleteAll();
-
-            user = new User("1234", "1234", "12345", Level.BASIC, 0, 0);
-
-            userDao.add(user);
-
-            user = new User("didii1l", "김길동", "12345", Level.BASIC, 0, 0);
-
-            userDao.add(user);
-
-            userService.upgradeLevels();
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-        
+        System.out.println("Test 시작");
     }
 }
