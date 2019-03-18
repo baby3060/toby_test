b834f66804e209b2215118be1c1856158e446f6e
psk <psk3060@naver.com>
2019-03-18 15:21:00 +0900

    트랜잭션 적용

--- src/main/java/com/tobexam/service/UserService.java
+++ src/main/java/com/tobexam/service/UserService.java
@@ -3,10 +3,18 @@ package com.tobexam.service;
 import com.tobexam.model.*;
 import com.tobexam.dao.*;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 import java.util.*;
 
+// 트랜잭션 동기화 관리자 => 동기화 작업 초기화하기 위한
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
 public class UserService {
-    UserDao userDao;
+    private UserDao userDao;
+    private DataSource dataSource;
 
     public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
     public static final int MIN_RECOMEND_FOR_GOLD = 30;
@@ -15,22 +23,35 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    public void upgradeLevels() {
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+
+    public void upgradeLevels() throws Exception {
+        TransactionSynchronizationManager.initSynchronization();
+        Connection c = DataSourceUtils.getConnection(dataSource);
+        c.setAutoCommit(false);
+
         try {
             List<User> users = userDao.selectAll();
-
             boolean changed = false;
-
             for( User user : users ) {
                 if( canUpgradeLevel(user) ) {
                     upgradeLevel(user);
                 }
             }
+            c.commit();
         } catch(Exception e) {
-            System.out.println("로깅 실시");
-            e.printStackTrace();
+            c.rollback();
+            throw e;
+        } finally {
+            // DB 커넥션 해제
+            DataSourceUtils.releaseConnection(c, this.dataSource);
+            // 동기화 작업 종료
+            TransactionSynchronizationManager.unbindResource(this.dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
         }
-        
     }
 
     private boolean canUpgradeLevel(User user) {
@@ -44,7 +65,7 @@ public class UserService {
         }
     }
 
-    private void upgradeLevel(User user) {
+    protected void upgradeLevel(User user) {
         
         user.upgradeLevel();
         try {
