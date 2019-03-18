136eda5901d8686e62b3020ddb2aa8eb4c3aa197
psk <psk3060@naver.com>
2019-03-18 15:21:41 +0900

    쓸모없는 예외 체크 제거

--- src/test/java/com/tobexam/UserDaoTest.java
+++ src/test/java/com/tobexam/UserDaoTest.java
@@ -83,85 +83,54 @@ public class UserDaoTest {
     @Test
     public void updateAndGet() {
         
-        try {
-            int count = userDao.countAll();
-
-            assertThat(count, is(3));
-
-            User user = userDao.get("1");
-
-            count = userDao.count("1");
-
-            assertThat(count, is(1));
-
-            assertThat(user.getName(), is("이길동"));
-            assertThat(user.getPassword(), is("12345"));
-            
-            user.setName("1234");
-            user.setPassword("12345");
-
-            userDao.update(user);
-
-            user = userDao.get("1");
-
-            assertThat(user.getName(), is("1234"));
-            assertThat(user.getPassword(), is("12345"));
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        int count = userDao.countAll();
+        assertThat(count, is(3));
+        User user = userDao.get("1");
+        count = userDao.count("1");
+        assertThat(count, is(1));
+        assertThat(user.getName(), is("이길동"));
+        assertThat(user.getPassword(), is("12345"));
+        
+        user.setName("1234");
+        user.setPassword("12345");
+        userDao.update(user);
+        user = userDao.get("1");
+        assertThat(user.getName(), is("1234"));
+        assertThat(user.getPassword(), is("12345"));   
     }
 
     @Test
     public void selectAll() {
-        try {
-            userDao.deleteAll();
-            List<User> users0 = userDao.selectAll();
-            assertThat(users0.size(), is(0));
-
-            userDao.add(user1);
-
-            List<User> users1 = userDao.selectAll();
-            assertThat(users1.size(), is(1));
-            checkSameUser(user1, users1.get(0));
-
-            userDao.add(user2);
-
-            List<User> users2 = userDao.selectAll();
-            assertThat(users2.size(), is(2));
-            checkSameUser(user1, users2.get(0));
-            checkSameUser(user2, users2.get(1));
-
-            userDao.add(user3);
-
-            List<User> users3 = userDao.selectAll();
-            assertThat(users3.size(), is(3));
-            checkSameUser(user1, users3.get(0));
-            checkSameUser(user2, users3.get(1));
-            checkSameUser(user3, users3.get(2));
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        userDao.deleteAll();
+        List<User> users0 = userDao.selectAll();
+        assertThat(users0.size(), is(0));
+        userDao.add(user1);
+        List<User> users1 = userDao.selectAll();
+        assertThat(users1.size(), is(1));
+        checkSameUser(user1, users1.get(0));
+        userDao.add(user2);
+        List<User> users2 = userDao.selectAll();
+        assertThat(users2.size(), is(2));
+        checkSameUser(user1, users2.get(0));
+        checkSameUser(user2, users2.get(1));
+        userDao.add(user3);
+        List<User> users3 = userDao.selectAll();
+        assertThat(users3.size(), is(3));
+        checkSameUser(user1, users3.get(0));
+        checkSameUser(user2, users3.get(1));
+        checkSameUser(user3, users3.get(2));
     }
 
     @Test
     public void update() {
-        try {
-            userDao.deleteAll();
-
-            userDao.add(user1); 
-
-            user1.setRecommend(1000);
-
-            userDao.update(user1);
-
-            User user1Update = userDao.get(user1.getId());
-
-            checkSameUser(user1, user1Update);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        
+        userDao.deleteAll();
+        userDao.add(user1); 
+        user1.setRecommend(1000);
+        userDao.update(user1);
+        User user1Update = userDao.get(user1.getId());
+        checkSameUser(user1, user1Update);
+       
     }
 
     private void checkSameUser(User target, User source) {
