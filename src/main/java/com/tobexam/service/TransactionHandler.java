3ef2c7924e8bde9a813f60a32de59cb4a9fe57ae
psk <psk3060@naver.com>
2019-03-19 19:12:45 +0900

    다이내믹 프록시를 이용하여 부가기능 제공

---
+++ src/main/java/com/tobexam/service/TransactionHandler.java
@@ -0,0 +1,50 @@
+package com.tobexam.service;
+
+import java.lang.reflect.*;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+
+public class TransactionHandler implements InvocationHandler {
+    // 부가기능(트랜잭션 경계 설정)을 추가할 객체
+    private Object target;
+    // 트랜잭션을 적용할 메소드
+    private String pattern;
+
+    private PlatformTransactionManager transactionManager;
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if( method.getName().startsWith(this.pattern) ) {
+            return invokeInTransaction(method, args);
+        } else {
+            return method.invoke(target, args);
+        }
+    }
+
+    private Object invokeInTransaction(Method method, Object[] args) throws Throwable {
+        TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            // 해당 메소드 호출
+            Object ret = method.invoke(target, args);
+            this.transactionManager.commit(status);
+            return ret;
+        } catch(InvocationTargetException e) {
+            this.transactionManager.rollback(status);
+            throw e.getTargetException();
+        }
+    }
+}
