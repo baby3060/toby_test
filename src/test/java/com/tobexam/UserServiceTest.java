8abb7bfd25bf3bc2a21ae913cb89879632984966
psk <psk3060@naver.com>
2019-03-19 16:13:10 +0900

    트랜잭션 테스트 수정

--- src/test/java/com/tobexam/UserServiceTest.java
+++ src/test/java/com/tobexam/UserServiceTest.java
@@ -101,6 +101,32 @@ public class UserServiceTest {
     }
 
     @Test
+    public void upgradeAllOrNothing() throws Exception {
+        TestUserService testUserService = new TestUserService(users.get(4).getId());
+        testUserService.setUserDao(userDao);
+        testUserService.setMailSender(mailSender);
+
+        UserServiceTx txUserService = new UserServiceTx();
+        txUserService.setTransactionManager(transactionManager);
+        txUserService.setUserService(testUserService);
+
+        userDao.deleteAll();
+
+        for(User user : users) {
+            userDao.add(user);
+        }
+
+        try {
+            txUserService.upgradeLevels();
+            fail("TestUserServiceException expected");
+        } catch(TestUserServiceException e) {
+            
+        }
+
+        checkLevelUpgraded(users.get(1), false);
+    }
+
+    @Test
     public void upgradeLevels() {
         userServiceImpl.deleteAll();
         for(User user : users) {
