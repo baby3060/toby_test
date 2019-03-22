2d7e1a68ea13fff2d1e2e0bab8fce69d89e32f05
psk <psk3060@naver.com>
2019-03-22 19:51:57 +0900

    Bean 후처리기 등록을 통한 SQL 초기화

--- src/main/java/com/tobexam/sqlconfig/XmlSqlService.java
+++ src/main/java/com/tobexam/sqlconfig/XmlSqlService.java
@@ -9,6 +9,8 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
+import javax.annotation.PostConstruct;
+
 /**
  * XML에서 SQL 읽어오기 위한 클래스
  */
@@ -21,13 +23,12 @@ public class XmlSqlService implements SqlService {
         this.sqlmapFile = sqlmapFile;
     }
 
-    public XmlSqlService() {
-        initSql();
-    }
-
     /**
      * SQL 초기화
+     * 이 애노테이션이 붙으면 스프링은 해당 클래스 Bean을 생성하고, DI 작업을 마친 뒤 이 애노테이션이 달린 메소드를 실행한다.
+     * XML 설정 읽기 => Bean 객체 생성 => DI => 후처리기 수행
      */
+    @PostConstruct
     public void initSql() {
         String contextPath = Sqlmap.class.getPackage().getName();
         try {
@@ -38,7 +39,7 @@ public class XmlSqlService implements SqlService {
             // XML을 자바 객체로 전환하기 위한 언마샬러
             Unmarshaller unmarshaller = context.createUnmarshaller();
 
-            Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(classLoader.getResourceAsStream(sqlmapFile));
+            Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(classLoader.getResourceAsStream(this.sqlmapFile));
 
             for( SqlType sql : sqlmap.getSql() ) {
                 sqlMap.put(sql.getKey(), sql.getValue());
