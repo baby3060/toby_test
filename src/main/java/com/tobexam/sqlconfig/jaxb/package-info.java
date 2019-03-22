2864c8798707197d59d31b9d2b319306f83a7b9b
psk <psk3060@naver.com>
2019-03-22 18:24:29 +0900

    JAXB 사용 결과

---
+++ src/main/java/com/tobexam/sqlconfig/jaxb/package-info.java
@@ -0,0 +1,9 @@
+//
+// 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.3.1 버전을 통해 생성되었습니다. 
+// <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a>를 참조하십시오. 
+// 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
+// 생성 날짜: 2019.03.22 시간 06:20:23 PM KST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.epril.com/sqlmap", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.tobexam.sqlconfig.jaxb;
