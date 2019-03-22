9c40491dde99f910923a58e30dbcb26c0f7d15fb
psk <psk3060@naver.com>
2019-03-22 19:13:48 +0900

    SQL 따로 분리 및 테스트

--- src/test/java/com/tobexam/JaxbTest.java
+++ src/test/java/com/tobexam/JaxbTest.java
@@ -30,14 +30,27 @@ public class JaxbTest {
 
         Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(classLoader.getResourceAsStream("sqlmap.xml"));
 
+        // 테스트클래스와 같은 위치 일 경우
+        // Sqlmap sqlmap = (Sqlmap)unmarshaller.unmarshal(getClass().getResourceAsStream("sqlmap.xml"));
+
         List<SqlType> sqlList = sqlmap.getSql();
 
-        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.size(), is(8));
         assertThat(sqlList.get(0).getKey(), is("add"));
-        assertThat(sqlList.get(0).getValue(), is("insert"));
-        assertThat(sqlList.get(1).getKey(), is("get"));
-        assertThat(sqlList.get(1).getValue(), is("select"));
-        assertThat(sqlList.get(2).getKey(), is("delete"));
-        assertThat(sqlList.get(2).getValue(), is("delete"));
+        assertThat(sqlList.get(0).getValue(), is("Insert Into USER(id, name, password, level, login, recommend, email) Values (?, ?, ?, ?, ?, ?, ?)"));
+        assertThat(sqlList.get(1).getKey(), is("deleteAll"));
+        assertThat(sqlList.get(1).getValue(), is("Delete From USER"));
+        assertThat(sqlList.get(2).getKey(), is("update"));
+        assertThat(sqlList.get(2).getValue(), is("Update USER set name = ?, password = ?, level = ?, login = ?, recommend = ?, email = ? Where id = ?"));
+        assertThat(sqlList.get(3).getKey(), is("delete"));
+        assertThat(sqlList.get(3).getValue(), is("Delete From USER Where id = ?"));
+        assertThat(sqlList.get(4).getKey(), is("get"));
+        assertThat(sqlList.get(4).getValue(), is("Select * From USER Where id = ?"));
+        assertThat(sqlList.get(5).getKey(), is("count"));
+        assertThat(sqlList.get(5).getValue(), is("Select Count(*) As cnt From USER Where id = ?"));
+        assertThat(sqlList.get(6).getKey(), is("countAll"));
+        assertThat(sqlList.get(6).getValue(), is("Select Count(*) As cnt From USER"));
+        assertThat(sqlList.get(7).getKey(), is("selectAll"));
+        assertThat(sqlList.get(7).getValue(), is("Select * From USER Order By id"));
     }
 }
