dd6272fb4ac07c646936c6a30920e82d8d5083b4
psk <psk3060@naver.com>
2019-03-24 17:29:47 +0900

    멀티스레드 안전을 위한 ConcrentHash 사용

---
+++ src/test/java/com/tobexam/ConcurrentHashMapSqlRegistryTest.java
@@ -0,0 +1,72 @@
+package com.tobexam;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+import com.tobexam.sqlconfig.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/test-applicationContext.xml")
+public class ConcurrentHashMapSqlRegistryTest {
+    UpdatableSqlRegistry sqlRegistry;
+
+    @Before
+    public void setUp() {
+        sqlRegistry = new ConcurrentHashMapSqlRegistry();
+
+        sqlRegistry.registerSql("KEY1", "SQL1");
+        sqlRegistry.registerSql("KEY2", "SQL2");
+        sqlRegistry.registerSql("KEY3", "SQL3");
+    }
+
+
+    @Test
+    public void find() {
+        checkFindResult("SQL1", "SQL2", "SQL3");
+    }
+
+    private void checkFindResult(String expected1, String expected2, String expected3) {
+        assertThat(sqlRegistry.findSql("KEY1"), is(expected1));
+        assertThat(sqlRegistry.findSql("KEY2"), is(expected2));
+        assertThat(sqlRegistry.findSql("KEY3"), is(expected3));
+    }
+
+    @Test(expected=SqlNotFoundException.class)
+    public void unknownKey() {
+        sqlRegistry.findSql("SQL9999");
+    }
+
+    @Test
+    public void updateSingle() {
+        sqlRegistry.updateSql("KEY2", "modified2");
+        checkFindResult("SQL1", "modified2", "SQL3");
+    }
+
+
+    @Test
+    public void updateMulti() {
+        Map<String, String> sqlmap = new HashMap<String, String>();
+
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY3", "Modified3");
+
+        sqlRegistry.updateSql(sqlmap);
+        checkFindResult("Modified1", "SQL2", "Modified3");
+    }
+
+    @Test(expected=SqlUpdateFailureException.class)
+    public void updateWithNotExistingKey() {
+        sqlRegistry.updateSql("SQL1901001", "Modified2");
+    }
+}
